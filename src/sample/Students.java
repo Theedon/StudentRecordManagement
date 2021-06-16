f7ee94f104402c454f06ce1858abefd3843dc833
Theedon <faniyantoyin@gmail.com>
2021-06-16 18:43:23 +0100

    added the new textfields to the Students class added constructors, getters and setters

--- src/sample/Students.java
+++ src/sample/Students.java
@@ -6,10 +6,14 @@ public class Students {
     private int serialNumber;
     private int id;
     private String firstName;
+    private String middleName;
     private String lastName;
     private String email;
     private String gender;
     private String student_class;
+    private String matric_no;
+    private String faculty;
+    private String department;
     private Image passport;
     private Image admission_img;
     private Image olevel_img;
@@ -17,22 +21,23 @@ public class Students {
     private Image jamb_img;
 
 
-
-    public Students(int serialNumber, int id, String firstName, String lastName, String email, String gender,
-                    Image passport, Image admission_img, Image olevel_img,
-                    Image guarantor_img, Image jamb_img) {
+    public Students(int serialNumber, int id, String firstName, String middleName, String lastName, String email, String gender, String student_class, String matric_no, String faculty, String department, Image passport, Image admission_img, Image olevel_img, Image guarantor_img, Image jamb_img) {
         this.serialNumber = serialNumber;
         this.id = id;
         this.firstName = firstName;
+        this.middleName = middleName;
         this.lastName = lastName;
-        this.email= email;
+        this.email = email;
         this.gender = gender;
-        this.student_class=  student_class;
+        this.student_class = student_class;
+        this.matric_no = matric_no;
+        this.faculty = faculty;
+        this.department = department;
         this.passport = passport;
-        this.admission_img= admission_img;
-        this.olevel_img= olevel_img;
-        this.guarantor_img= guarantor_img;
-        this.jamb_img= jamb_img;
+        this.admission_img = admission_img;
+        this.olevel_img = olevel_img;
+        this.guarantor_img = guarantor_img;
+        this.jamb_img = jamb_img;
     }
 
     public int getSerialNumber() {
@@ -59,6 +64,14 @@ public class Students {
         this.firstName = firstName;
     }
 
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
     public String getLastName() {
         return lastName;
     }
@@ -67,12 +80,12 @@ public class Students {
         this.lastName = lastName;
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email){
-        this.email= email;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getGender() {
@@ -83,6 +96,37 @@ public class Students {
         this.gender = gender;
     }
 
+    public String getStudent_class() {
+        return student_class;
+    }
+
+    public void setStudent_class(String student_class) {
+        this.student_class = student_class;
+    }
+
+    public String getMatric_no() {
+        return matric_no;
+    }
+
+    public void setMatric_no(String matric_no) {
+        this.matric_no = matric_no;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 
     public Image getPassport() {
         return passport;
@@ -123,6 +167,4 @@ public class Students {
     public void setJamb_img(Image jamb_img) {
         this.jamb_img = jamb_img;
     }
-
-
 }
